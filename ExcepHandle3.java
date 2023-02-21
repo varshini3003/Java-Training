e25e86bbc62259ce48fb7f2eade3a00fe432d576
Varshini <varshini.sanakr@cdw.com>
2023-02-21 20:26:39 +0530

    Added new file

---
+++ ExcepHandle3.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+/*
+ * Try the code by replacing the return statement in the try block with System.exit(0)
+ * Output is - "catch-method2 finally-method2 caller"
+ */
+class Sample
+{
+    public void method1()
+    {
+        method2();
+        System.out.println("caller");
+    }
+    public void method2()
+    {
+        try
+        {
+            //return; 
+            System.exit(0);
+        }
+        catch(Exception e)
+        {
+            System.out.println("catch-method2");  
+        }
+        finally
+        {
+            System.out.println("finally-method2");
+        }
+    }
+}
+public class ExcepHandle3 {
+    public static void main(String[] args) {
+    Sample s = new Sample();
+    s.method1();
+}
+}
+
