e3fe1a85dbfd65a233599ccc33a3d9e96cce5ac2
Varshini <varshini.sanakr@cdw.com>
2023-02-21 08:08:04 +0530

    Added few changes

--- Ques8.java
+++ Ques8.java
@@ -3,27 +3,26 @@
  */
 class Single 
 {   
-    private static Single s = null;   
-    public String str;   
+    static Single s = new Single();     
+    public char ch = 'a'; 
     private Single()   
     {   
-        str = "Learning singleton class.";   
+        System.out.println("Learning singleton class...");   
     }     
+    //Global access to get the object of Singleton class
     public static Single getObject()   
     {   
-        if (s== null)   
-        {
-            s = new Single();   
-        }
         return s;   
     }   
 }  
 public class Ques8 {
     public static void main(String[] args)
     {
-        Single a = Single.getObject();   
-        Single b = Single.getObject();   
-        System.out.println(a.str);
-        System.out.println(b.str);
+        Single a = Single.getObject(); 
+        a.ch+=5; 
+        System.out.println(a.ch); 
+        Single b = Single.getObject();  
+        System.out.println(b.ch);
+        System.out.println("a and b refer to the same object...");
     }
 }
