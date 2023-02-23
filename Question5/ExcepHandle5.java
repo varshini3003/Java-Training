1cc4f1e9bb686719d2f088f346d3ecd92ba16de3
varshini3003 <125632109+varshini3003@users.noreply.github.com>
2023-02-23 23:21:57 +0530

    Update ExcepHandle5.java

--- Question5/ExcepHandle5.java
+++ Question5/ExcepHandle5.java
@@ -7,8 +7,8 @@ public class ExcepHandle5 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a phone number");
         String phno = scanner.nextLine();
-        MyException me = new MyException();
-        me.validatePhoneNumber(phno);
+        MyException my_exception = new MyException();
+        my_exception.validatePhoneNumber(phno);
         /* Catch not included...the exception is not caught (within a catch block), so
         * the runtime system ends the program and prints the exception message.
         */
