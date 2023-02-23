161c8804dd55443698e56afd88378edea0591831
Varshini <varshini.sanakr@cdw.com>
2023-02-23 22:37:24 +0530

    Added few changes

---
+++ Ques_1/Question2.java
@@ -0,0 +1,20 @@
+package Ques_1;
+
+/*
+ * Printing initial values of each data type
+ */
+public class Question2 {
+	static int in;
+	static float fl;
+	static char ch;
+	static String str;
+	static byte b;
+	static long l;
+	static boolean bool;
+	static double d;
+	static short s;
+	public static void main(String[] args)
+	{
+		System.out.print("Integer: "+in+", Float: "+fl+", Char: "+ch+", String: "+str+", Byte: "+b+", Long: "+l+", Boolean: "+bool+", Double: "+d+", Short: "+s);
+	}
+}
