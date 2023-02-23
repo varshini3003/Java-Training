203351802a407d3444cdcde9dd8e53022bc4232a
Varshini <varshini.sanakr@cdw.com>
2023-02-23 22:37:59 +0530

    Added few changes

---
+++ Ques_5/C1.java
@@ -0,0 +1,10 @@
+package Ques_5;
+
+class C1 extends A1
+{
+    B1 objB = new B1();
+    void print()
+    {
+        System.out.println("Created a member of object B in class C...Value of b: "+objB.b);
+    }
+}
