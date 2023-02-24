33dd2130b6989860fd85789090c0e1e8d45dab8f
Varshini <varshini.sanakr@cdw.com>
2023-02-24 17:20:32 +0530

    Made few changes

--- Thread1/Numbers.java
+++ Thread1/Numbers.java
@@ -2,20 +2,24 @@ package Thread1;
 
 public class Numbers {
     synchronized public void displayEven() {
-	Thread t=Thread.currentThread();
-	String name=t.getName();
-	System.out.println(name);
+        Thread t = Thread.currentThread();
+        String name = t.getName();
+        System.out.println(name);
         for(int num=2; num<=100; num+=2)
         {
             System.out.print(num+" ");
         }
         System.out.println();
-	try {Thread.sleep(2000);}catch(Exception e) {}
+        try
+        {
+            Thread.sleep(2000);
+        }
+        catch(Exception e) {}
     }
     synchronized public void displayOdd() {
-	Thread t=Thread.currentThread();
-	String name=t.getName();
-	System.out.println(name);
+        Thread t = Thread.currentThread();
+        String name = t.getName();
+        System.out.println(name);
         for(int num=1; num<=100; num+=2)
         {
             System.out.print(num+" ");
