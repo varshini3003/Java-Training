c0a20e0b9b47f0ddddc74f82c52b237100bada0b
Varshini <varshini.sanakr@cdw.com>
2023-02-28 23:50:50 +0530

    Added java doc

--- Thread6/MyException.java
+++ Thread6/MyException.java
@@ -2,6 +2,11 @@ package Thread6;
 
 public class MyException {
     int total=0;
+    
+    /** 
+     * @param character  Character read from the user
+     * @throws InvalidAlphabetException If the read character is not an alphabet
+     */
     public void totalLetters(char character) throws InvalidAlphabetException
     {
         if(Character.isAlphabetic(character))
