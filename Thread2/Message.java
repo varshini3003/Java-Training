6352cf5a4cbf1994e84e4b7c121b87409773aa2e
Varshini <varshini.sanakr@cdw.com>
2023-02-28 23:50:03 +0530

    Added java doc

--- Thread2/Message.java
+++ Thread2/Message.java
@@ -1,6 +1,10 @@
 package Thread2;
 
 public class Message {
+    
+    /** 
+     * @param str String read from the user to display it in the given format
+     */
     synchronized public void display(String str)
     {
         System.out.println("------"+str+"------");
