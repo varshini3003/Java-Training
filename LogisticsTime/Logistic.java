eeceb055d3181e8fd9edfdf2fd85b4e68dca808c
Varshini <varshini.sanakr@cdw.com>
2023-03-03 20:04:55 +0530

    Added new file

--- LogisticsTime/Logistic.java
+++ LogisticsTime/Logistic.java
@@ -3,6 +3,15 @@ package LogisticsTime;
 import java.util.*;
 import java.time.*;
 public class Logistic {
+    public static boolean isSunday(LocalDate localDate)
+    {
+        String dayOfWeek = localDate.getDayOfWeek().toString();
+        if("SUNDAY".equalsIgnoreCase(dayOfWeek))
+        {
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the distance in kms");
@@ -14,10 +23,38 @@ public class Logistic {
 		System.out.println(hour+":"+minute);
 	    LocalDate newDate = null;
 	    LocalDate localDate = LocalDate.now();
-		int month = LocalDate.now().getDayOfMonth();
+        Month currMonth = localDate.getMonth();
+        String month = currMonth.toString();
+		int dayOfMonth = LocalDate.now().getDayOfMonth();
 		DayOfWeek day = localDate.getDayOfWeek();
 		int dayOfYear = LocalDate.now().getDayOfYear();
-		System.out.print(localDate+ " "+month+" "+day+" "+dayOfYear);
-		String year = localDate.toString().substring(0,5);
+        int year = LocalDate.now().getYear();
+		System.out.println(localDate+ " "+dayOfMonth+" "+day+" "+dayOfYear+" "+year+" "+month);
+        double timeTaken = distance/speed;
+        System.out.println("Time taken in hrs: "+ timeTaken);
+        int distanceCoveredPerDay = speed*8;
+        System.out.println(distanceCoveredPerDay);
+        int distanceCovered=distanceCoveredPerDay;
+        int actualTimeTaken=0;
+        while(true)
+        {
+            if(distanceCovered==distance)
+            {
+                
+                break;
+            }
+            if(month.equals("JANUARY") && dayOfMonth==1 || dayOfMonth==26)
+            {
+                
+            }
+            else if(month.equals("AUGUST") && dayOfMonth==15)
+            {
+                
+            }  
+            distanceCoveredPerDay++;
+            actualTimeTaken+=8;
+        }
+        System.out.println(distanceCovered);
+
     }
 }
