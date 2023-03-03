5bf7513cd7437ac90c2f647130a861acdb766e4e
Varshini <varshini.sanakr@cdw.com>
2023-03-03 14:11:46 +0530

    Made few changes

--- Momento/MomentoDemo.java
+++ Momento/MomentoDemo.java
@@ -4,10 +4,10 @@ public class MomentoDemo {
     public static void main(String[] args) 
     {
         TextEditor textEditor = new TextEditor(new TextWindow());
-        textEditor.write("The Memento Design Pattern\n");;
-        textEditor.write("How to implement it in Java?\n");
+        textEditor.write("Memento Design Pattern\n");;
+        textEditor.write("Learning...\n");
         textEditor.hitSave();        
-        textEditor.write("Buy milk and eggs before coming home\n");    
+        textEditor.write("Testing Undo and Redo text...\n");    
         System.out.println(textEditor.print());
         textEditor.hitUndo();
         System.out.println(textEditor.print());
