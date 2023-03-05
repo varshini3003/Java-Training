1fdf864b84cb137f92522849f92b05434443793a
Varshini <varshini.sanakr@cdw.com>
2023-03-05 14:03:19 +0530

    Added new file

--- StdInUsage/StdInDemo.java
+++ StdInUsage/StdInDemo.java
@@ -2,14 +2,15 @@ package StdInUsage;
 /*
  * Count the number of characters entered through StdIn
  */
-import java.io.*;
+import java.util.*;
 public class StdInDemo {
     public static void main(String[] args) {
         int characters=0;
-        while(!StdIn.isEmpty())
+        Scanner scanner = new Scanner(System.in);   
+        while(scanner.hasNext())
         {
-            String string = StdIn.readString();
-            
+            char character = scanner.next().charAt(0);
+            characters++;
         }
         System.out.println("Number of characters are "+characters);
     }
